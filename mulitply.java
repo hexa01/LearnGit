7b881e8beb9e2a31578cc970cc7521db56f2e50b
Sushan Poudel <sushanpoudel75@gmail.com>
2023-04-28 15:20:11 +0545

    dev branch first commit

--- mulitply.java
+++ mulitply.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
-public class diff {
+import java.util.*;
+public class mulitply {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int diff = a - b;
-        System.out.println("The sum of a and b is: " + diff);
+        int answer = a * b;
+        System.out.println("The sum of a and b is: " + answer);
         sc.close();
      }
 }
-
