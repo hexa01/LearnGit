869a9354e64d7bbe79d813089dad1d9601b66113
Sushan Poudel <sushanpoudel75@gmail.com>
2023-04-28 14:59:58 +0545

    third commit

--- sum.java
+++ sum.java
@@ -5,7 +5,7 @@ public class sum{
     int a = sc.nextInt();
     int b = sc.nextInt();
     int sum = a + b;
-    System.out.println(sum);
+    System.out.println("The sum of a and b is: " + sum);
     sc.close();
  }
 }
